d47781780914d486819712f1365a94a72c1fe707
kaustubhkashyap23 <kaustubhkashyap23@gmail.com>
2017-09-19 19:45:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hello';
 //Move the image
 var img = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginLeft = '100px';
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft+1;
+    img.style.marginLeft = marginLeft + 'px';
 }
+img.onclick = function() {
+    var interval = setInterval(moveRight,50);
+};
