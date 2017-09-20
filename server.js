9ae68776c358dfac09a96bb222035b40ce055f48
kaustubhkashyap23 <kaustubhkashyap23@gmail.com>
2017-09-21 01:05:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,12 +93,10 @@ app.get('/:articleName', function(req,res) {
     res.send(createTemplate(articles[articleName]));
 });
     
-app.get('/article-two', function(req,res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+var counter = 0;
+app.get('/counter',function(req,res) {
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
